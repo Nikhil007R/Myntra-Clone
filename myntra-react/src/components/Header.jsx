fb2905628a1b8dfdcdfba906524b60ed251ca659
Nikhil Rawat <nikhilrawaa311@gmail.com>
2024-10-13 11:35:27 +0530

    routing done.

--- myntra-react/src/components/Header.jsx
+++ myntra-react/src/components/Header.jsx
@@ -1,19 +1,20 @@
 import { BsPersonFill } from "react-icons/bs";
 import { GiHeartBeats } from "react-icons/gi";
 import { FaShoppingBag } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
       <header>
         <div className="logo_container">
-          <a href="#">
+          <Link to="/">
             <img
               className="myntra_home"
               src="images/myntra_logo.webp"
               alt="Myntra Home"
             />
-          </a>
+          </Link>
         </div>
         <nav className="nav_bar">
           <a href="#">Men</a>
@@ -43,11 +44,11 @@ const Header = () => {
             <span className="action_name">Wishlist</span>
           </div>
 
-          <a className="action_container" href="pages/bag.html">
+          <Link className="action_container" to="/Bag">
             <FaShoppingBag />
             <span className="action_name">Bag</span>
             <span className="bag-item-count">0</span>
-          </a>
+          </Link>
         </div>
       </header>
     </>
